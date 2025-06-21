17bfc17f807e54cf81143d163224223ae370801a
Maksgnap <107640396+Maksimgnam@users.noreply.github.com>
2025-06-21 12:54:36 +0300

    add ui

---
+++ src/app/account/page.js
@@ -0,0 +1,12 @@
+import Account from '@/components/Account'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-svh '>
+        <Account/>
+    </div>
+  )
+}
+
+export default page
