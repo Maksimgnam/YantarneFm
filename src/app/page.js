cc9ffa2f0e59676eb0a5bb14c0a8850ce6102fcb
Maksgnap <107640396+Maksimgnam@users.noreply.github.com>
2025-08-18 09:05:05 +0300

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
-
-// import { useRouter } from "next/navigation";
-import Image from "next/image";
-import Link from "next/link";
+import Loader from "@/components/Loader/Loader";
 
 export const metadata = {
   title: "Yantarne FM - радіо рідного міста",
@@ -12,10 +9,8 @@ export default function Page() {
 
 
   return (
-    <main className='w-full h-full flex  items-center justify-center p-5'>
-      <Link href='/home'>
-        <Image src={'/logo.webp'} width={190} height={190} alt='' className='rounded-3xl cursor-pointer' />
-      </Link>
+    <main className='w-full h-full'>
+      <Loader/>
   </main>
      
   );
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,35 @@
+'use client';
+import React, { useEffect } from 'react';
+import './Loader.scss';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const Loader = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/home');
+    }, 3000); 
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <main className='loader'>
+      <Image 
+        src={'/logo.webp'} 
+        width={340} 
+        height={340} 
+        alt='Logo' 
+        className='m-4' 
+      />
+
+      <div className='loading-line-container'>
+        <div className="loading-line"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Loader;
