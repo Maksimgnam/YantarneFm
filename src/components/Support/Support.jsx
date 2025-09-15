568d2f87554529a3d7acf3765525ccf2c57e3d37
Maksgnap <107640396+Maksimgnam@users.noreply.github.com>
2025-09-15 13:48:54 +0300

    some updates

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Support = () => {
 
-  const [news, setNews] = useState({text: "loading...", url: "loading..."});
+  const [news, setNews] = useState({text: "Завантаження..", url: "Завантаження..."});
 
   const fetchNews = async () => {
     try {
--- src/components/Tapalka/Tapalka.jsx
+++ src/components/Tapalka/Tapalka.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './Tapalka.scss'
+'use client'
+import React, { useState } from 'react';
+import './Tapalka.scss';
 
 const Tapalka = () => {
-  return (
-    <main className='w-full h-svh'>Tapalka</main>
-  )
-}
+  const [tap, setTap] = useState(0);
 
-export default Tapalka
+  return (
+    <main className="w-full h-svh flex flex-col items-center justify-center gap-4">
+      {/* <h1 className="text-4xl font-bold">{tap}</h1>
+      
+      <div className="flex gap-3">
+        <button 
+          onClick={() => setTap(tap + 1)} 
+          className="px-4 py-2 bg-green-500 cursor-pointer text-white rounded-xl hover:bg-green-600 transition"
+        >
+          +
+        </button>
+      </div> */}
+    </main>
+  );
+};
+
+export default Tapalka;
