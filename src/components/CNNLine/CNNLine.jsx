3a54ee452e2864eccb0c00846fc6352fb55ff55e
Maksgnap <107640396+Maksimgnam@users.noreply.github.com>
2025-09-17 15:57:06 +0300

    some fixes

--- src/components/CNNLine/CNNLine.jsx
+++ src/components/CNNLine/CNNLine.jsx
@@ -59,7 +59,7 @@ const CNNLine = () => {
   // fallback якщо API порожній
   const items = cnnTexts.length
     ? cnnTexts.map(item => item.text).join(' • ')
-    : ["Нова частота 97.6 FM", "Yantarne FM", "Фестивалі", "Новини"];
+    : ["Нова частота 97.6 FM", "Yantarne FM", "Фестивалі", "Новини","Нова частота 97.6 FM", "Yantarne FM", "Фестивалі", "Новини","Нова частота 97.6 FM", "Yantarne FM", "Фестивалі", "Новини","Нова частота 97.6 FM", "Yantarne FM", "Фестивалі", "Новини" ];
 
   // множимо, щоб було багато тексту
   const repeatedItems = Array(1).fill(items).flat();
